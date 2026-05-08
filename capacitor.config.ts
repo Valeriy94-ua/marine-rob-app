61dbd03f7dace8d1369d911618a5eefaff746987
Valeriy94-ua <128257210+Valeriy94-ua@users.noreply.github.com>
2026-05-08 14:59:39 +0300

    Full screen mobile

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 ﻿import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.marinerob.app',
+  appId: 'com.valeriy.marinerob',
   appName: 'Marine ROB',
   webDir: 'dist'
 };
